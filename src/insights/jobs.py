17ba5c9bb993ceff2d30f23316c2fdf7602c5567
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-03 12:52:56 -0300

    commit 4 req

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -16,5 +16,24 @@ class ProcessJobs:
     def get_unique_job_types(self) -> List[str]:
         return list({job["job_type"] for job in self.jobs_list})
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(
+        self, jobs_dict, filters_dict
+    ) -> List[dict]:
+        if not isinstance(filters_dict, dict):
+            raise TypeError("The filter must be a dict")
+        industryFilter = filters_dict.get("industry") is None
+        jobFilter = filters_dict.get("job_type") is None
+
+        return [
+            job
+            for job in jobs_dict
+            if (
+                industryFilter
+                or job.get("industry") == filters_dict.get("industry")
+            )
+            and (
+                jobFilter
+                or job.get("job_type") == filters_dict.get("job_type")
+            )
+        ]
+        # pass
