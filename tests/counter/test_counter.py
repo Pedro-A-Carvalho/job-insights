fd2133d36b1ff4be5a4548f8fd13922fa17169fb
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-03 11:49:16 -0300

    commit start

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    assert count_ocurrences("data/jobs.csv", "test") == 2687
+    assert count_ocurrences("data/jobs.csv", "data") == 23149
+    assert count_ocurrences("data/jobs.csv", "job") == 3454
+    assert count_ocurrences("data/jobs.csv", "Job") == 3454
