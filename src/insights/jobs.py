092f408da71a4905ec19787906bb309291d23aad
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-03 12:11:12 -0300

    commit 2 req

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path) -> List[Dict]:
+        with open(path, "r") as file:
+            reader = csv.DictReader(file)
+            self.jobs_list = list(reader)
+        return self.jobs_list
+        # pass
 
     def get_unique_job_types(self) -> List[str]:
         pass
