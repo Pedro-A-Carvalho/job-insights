370bdbed43a19899511290f350727ea66bb56e91
Pedro Almeida Carvalho <cont.pacarvalho@gmail.com>
2024-05-03 15:41:48 -0300

    commit 10 req

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    raise
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    # Assert
+    for job in result:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
